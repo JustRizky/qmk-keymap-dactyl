c48992ff4353640434cbc0a05833d6b9c23aeb46
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-07-13 03:25:04 +0700

    Update keymap.c

--- default/keymap.c
+++ default/keymap.c
@@ -32,12 +32,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6(
         _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
-        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
-        _______, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  ,                    KC_MINS, KC_EQL , KC_GRV , KC_LBRC, KC_RBRC, KC_PIPE,
-        KC_LSFT, KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  ,                    KC_UNDS, KC_PLUS, KC_TILD, KC_LCBR, KC_RCBR, KC_RSFT,
+        _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                    KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
+        _______, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,                    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______,
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
                           _______, _______,                                                        _______, _______,
-                                            _______,ALT_T(KC_ENT),               ALL_T(KC_SPC), KC_BSPC,
-                                            KC_LCMD, _______,                    _______, _______,
+                                            _______, _______,                    _______, _______,
+                                            _______, _______,                    _______, _______,
                                             _______, _______,                    _______, _______
     )
 };
