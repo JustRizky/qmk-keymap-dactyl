6744834d47d1205853431e72991b5bef5f69c59d
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-07-14 01:26:00 +0700

    Update keymap.c

--- default/keymap.c
+++ default/keymap.c
@@ -15,18 +15,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                    KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                           _______, _______,                                                        _______, _______,
                                             LOWER  ,ALT_T(KC_ENT),               ALL_T(KC_SPC), KC_BSPC,
-                                            KC_LGUI, _______,                    _______,  RAISE ,
+                                            _______, KC_LGUI,                    RAISE  , _______,
                                             _______, _______,                    _______, _______
     ),
 
     [_LOWER] = LAYOUT_5x6(
         _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
         _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
-        _______, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  ,                    KC_MINS, KC_EQL , KC_GRV , KC_LBRC, KC_RBRC, KC_PIPE,
-        KC_LSFT, KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  ,                    KC_UNDS, KC_PLUS, KC_TILD, KC_LCBR, KC_RCBR, KC_RSFT,
+        _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                    KC_MINS, KC_EQL , KC_GRV , KC_LBRC, KC_RBRC, KC_PIPE,
+        KC_LSFT, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,                    KC_UNDS, KC_PLUS, KC_TILD, KC_LCBR, KC_RCBR, KC_RSFT,
                           _______, _______,                                                        _______, _______,
                                             _______,ALT_T(KC_ENT),               ALL_T(KC_SPC), KC_BSPC,
-                                            KC_LGUI, _______,                    _______, _______,
+                                            _______, KC_LGUI,                    _______, _______,
                                             _______, _______,                    _______, _______
     ),
 
